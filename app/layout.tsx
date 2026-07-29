6986356575e2d792b8ee8604676482ba7b977bff
Aishwarya Raj <3spacetechcorp@gmail.com>
2026-07-29 17:31:37 +0530

    Add Organization Schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import Script from "next/script"
 import type React from "react"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "app\globals.css"
 // import CustomCursor from "@/components/CustomCursor"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -26,10 +27,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        
-        {/* <CustomCursor /> */}
-        {children}
-      </body>
+
+  <Script
+    id="organization-schema"
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        name: "Anjanisutah 3Space Pvt. Ltd.",
+        alternateName: "3Space",
+        url: "https://a3spacetech.com",
+        logo: "https://a3spacetech.com/logo.png",
+        email: "contactus@a3spacetech.com",
+        sameAs: [
+          "https://www.linkedin.com/company/3space-aerospace-and-space-technologies-company/",
+          "https://www.instagram.com/anjanisutah_3space",
+          "https://x.com/3Space_tech",
+          "https://youtube.com/@anjanisutah_3space"
+        ]
+      })
+    }}
+  />
+
+  {children}
+
+</body>
     </html>
   )
 }
